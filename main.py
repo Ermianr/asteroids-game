abfd0fe1b49f0e322025eb4894873c2a92ff25b4
Ermian <ermianrazor@gmail.com>
2024-12-30 16:48:31 -0500

    Se crearon las clases principales como CircleShape y player para renderizar el jugador y se ajustaron algunos valores

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
   pygame.init()
+
+  # Reloj de juego
+  clock = pygame.time.Clock()
+  dt = 0
+
+  # Crear jugador
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT /2
+  player = Player(x, y, PLAYER_RADIUS)
 
   # Crear la pantalla
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   while True:
      # Listener de eventos
-     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-           return quit
-     # Rellenar de negro la pantalla
-     screen.fill((0,0,0))
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          return quit
+      
 
-     # Actualizar la pantalla
-     pygame.display.flip()
+    # Rellenar de negro la pantalla
+    screen.fill((0,0,0))
+
+    # Dibujar jugador
+    player.draw(screen)
+
+    # Actualizar la pantalla
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
